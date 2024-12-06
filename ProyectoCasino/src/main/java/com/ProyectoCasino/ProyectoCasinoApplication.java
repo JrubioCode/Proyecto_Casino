9633be53300f22b313702c45197fea7bbdb1b8c9
JrubioCode <javii.central.7@gmail.com>
2024-12-06 11:30:14 +0100

    Estructura Proyecto Casino

---
+++ ProyectoCasino/src/main/java/com/ProyectoCasino/ProyectoCasinoApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoCasino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCasinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCasinoApplication.class, args);
+	}
+
+}
---
+++ ProyectoCasino/src/test/java/com/ProyectoCasino/ProyectoCasinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoCasino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCasinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
